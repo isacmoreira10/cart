19fb9e0b79d8d113901c0fd23168fb13483a04b6
isac moreira <108782511+isacmoreira10@users.noreply.github.com>
2023-03-06 18:50:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let name = document.querySelector('#name')
 let amount = document.querySelector('#amount')
 let value = document.querySelector('#price')
 let cartItems = []
+let divs = []
 
 
 function add() {
@@ -12,8 +13,8 @@ function add() {
         amount: Number(amount.value),
         value: Number(value.value)
     }
-    cartItems.push(item)
 
+    cartItems.push(item)
     output()
     total()
 }
@@ -23,13 +24,21 @@ function output() {
     for (let index = 0; index < cartItems.length; index++) {
         const htmlElement = `<div class="itens-p">
         <img src="Vector.png" class="image">
-        <p class="name">${cartItems[index].name}</p>
-        <p class="amount">${cartItems[index].amount + 'x'}</p>
-        <p class="value">${' R$' + cartItems[index].value + ' ,00 '}</p>
+        <p class="name" id="item">${cartItems[index].name}</p>
+        <p class="price">${cartItems[index].amount + 'x'}</p>
+        <p class="value">${' R$' + cartItems[index].value + ' ,00 ' + '<hr class="lineBreak">'}</p>
         </div>`
         stringCart += htmlElement
     }
     display.innerHTML = stringCart
+    divs.push(stringCart)
+
+    for (let i = 0; i < divs.length; i++) {
+        if (i === 6) {
+            alert("Adicione apenas 6 itens em sua compra")
+            document.location.reload(true)
+        }
+    }
 }
 
 function total() {
@@ -38,9 +47,14 @@ function total() {
         count = count + (cartItems[i].value * cartItems[i].amount)
     }
 
-    const total = `<div class="total">
-    <p class="count">${'R$' + [count] + ',00'}</p>
-    </div>`
+    const total = `<div class="result">
+        <p class="count">${'R$' + [count] + ',00'}</p>
+        </div>`
     results.innerHTML = total
-
 }
+
+
+
+
+
+
